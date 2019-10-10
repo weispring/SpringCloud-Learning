0791c9b51cbabe65cde091b246266bc88ed5de08
li.xianchun <li.xianchun@vpclub.cn>
2019-10-10 11:42:44 +0800

    rename project

--- Brixton/Chapter1-1-1/eureka-server/src/main/java/com/didispace/Application.java
+++ Brixton/Chapter1-1-1/eureka-server/src/main/java/com/didispace/Application.java
@@ -1,5 +1,6 @@
 package com.didispace;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Application {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		SpringApplication.run(Application.class,args);
 	}
 
 }
---
+++ Brixton/Chapter1-1-2/eureka-feign/src/main/java/com/didispace/service/TestClient.java
@@ -0,0 +1,19 @@
+package com.didispace.service;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+//@FeignClient(value = "compute-service")
+public interface TestClient {
+/*
+    @ApiOperation(value = "查询字典列表（下拉列表等使用）", notes = "层级字段hierarchy由上级的hierarchy和code通过,隔开组成,这个查询方法将查出其本级和其下级所有的数据")
+    @PostMapping(value = "/rpc/auth/sysmgt/dictionary/listDictionary")
+    VpResp<List<DictionaryRpcRespBody>> listDictionary(
+            @RequestBody @Validated VpReq<DictionarySimpleReqBody> req);*/
+
+}
